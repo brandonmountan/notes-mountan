27f359236a73e92ebc9c1782ab7bc69c742e80b1
Brandon Mountan <brandonmountan1@gmail.com>
2023-07-06 12:54:34 -0600

    read and write working

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const noteData = require('./db/db.json');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3001;
@@ -11,21 +12,55 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
     res.json(noteData);
 
-    console.info(`${req.method} request recieved`)
+    // console.info(`${req.method} request recieved`)
 });
 
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request recieved`)
+    // console.info(`${req.method} request recieved`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+        };
+
+        fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+
+                parsedNotes.push(newNote);
+
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4),
+                (writeErr) =>
+                writeErr ? console.error(writeErr) : console.info('Successfully updated notes')
+                );
+            }
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
 });
 
 app.listen(PORT, () => 
